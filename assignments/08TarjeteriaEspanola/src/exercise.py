a008220128bcdea9fab206507ae1d31990aed256
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-08-30 18:24:07 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,21 @@
+def ntcpli (a):
+    c= a*12
+    return c
+
+def ntcplu (b):
+    d= b*35
+    return d
 
 def main():
     #escribe tu código abajo de esta línea
+    pliegos= int(input("Dame la cantidad de pliegos de papel albanene: "))
+    plumones= int(input("Dame la cantidad de plumones: "))
+    z= ntcpli(pliegos)
+    x= ntcplu (plumones)
+    if z<x:
+        print ("El número máximo de tarjetas que se pueden hacer es: ",z)
+    elif x<z:
+        print ("El número máximo de tarjetas que se pueden hacer es:",x)
     pass
 
 if __name__=='__main__':
