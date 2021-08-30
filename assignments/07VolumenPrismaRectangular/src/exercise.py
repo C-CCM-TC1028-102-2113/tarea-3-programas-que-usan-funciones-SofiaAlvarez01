314c18e71b1eb5fbb2346af41eb7076050b22da0
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-08-30 18:01:17 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -7,12 +7,15 @@ def volumenrec (c,d):
     volumen= c*d
     return volumen
 
-base= float(input("Dame la base: "))
-altura= float(input("Dame la altura: "))
-profundidad= float(input("Dame la profundidad: "))
-y= arearec (base,altura)
-x= volumenrec (y, profundidad)
-print("El volumen del prisma es: ",x)
+def main():
+    base= float(input("Dame la base: "))
+    altura= float(input("Dame la altura: "))
+    profundidad= float(input("Dame la profundidad: "))
+    y= arearec (base,altura)
+    x= volumenrec (y, profundidad)
+    print("El volumen del prisma es: ",x)
+    
+main()
     
     pass
 
