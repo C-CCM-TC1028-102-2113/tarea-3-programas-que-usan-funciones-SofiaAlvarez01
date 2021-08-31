da342e4450e0777b957fec4a316ae46be1d1899b
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-08-30 19:27:58 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,17 @@
-
+def bisiesto (a):
+    if (a%4==0 and a%100!=0) or (a%400==0):
+        añof= 1
+    else:
+        añof= 2
+    return añof
 def main():
     #escribe tu código abajo de esta línea
+    año= int(input())
+    x= bisiesto (año)
+    if x==1:
+        print ("True")
+    elif x==2:
+        print("False")    
     pass
 
 if __name__=='__main__':
